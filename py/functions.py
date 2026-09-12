6a2d948871481a0e981e4a28432a1422859ab45d
Ruan Ferraz <ruanpabloferraz007curujinha@gmail.com>
2026-09-12 11:36:44 -0300

    try make run

--- py/functions.py
+++ py/functions.py
@@ -21,77 +21,16 @@ def cadastrar_tecla(tecla, texto):
     with open("py/config.json", "w", encoding="utf-8") as arq_2:
         json.dump(funcoes, arq_2, indent=4)
 
-def rodar_terminal():
-    print("+===============================+")
-    print("|      *    CADASTRAR    *      |")
-    print("|===============================|")
-    print("| Como deseja cadastrar?        |")
-    print("|-------------------------------|")
-    print("|  1- Tecla --> Texto / Atalho  |")
-    print("|  2- Atalho --> Texto          |")
-    print("|  3- REMAP                     |")
-    print("|  0- Cancelar                  |")
-    print("|-------------------------------|")
-    print("+===============================+")
-    print('  ------#OBS: O atalho deve seguir o seguinte formato: "key1+key2"')
-    print("")
-
+def leitura_tecla():
+    time.sleep(0.5)
+    print(">>>> Digite a tecla:")
+    while keyboard.read_event(suppress=False) and False:
+        pass
     while True:
-        print("Escolha uma opção:")
-        escolha = int(input())
-        if escolha < 0 or escolha > 3:
-            print("Opção inexistente!")
-            print("digite novamente.....")
-            print("")
-        else:
+        event = keyboard.read_event(suppress=False)
+        if event.event_type == "down":
+            chave = event.name
+            print(f"Tecla identificada: [{chave.upper}]!")
+            time.sleep(0.2)
             break
-
-    match escolha:
-        case 1:
-            print("Digite a tecla:")
-            event = keyboard.read_event()
-            key = event.name
-            time.sleep(0.5)
-            print(f"Tecla identificada: {key}!")
-            print("Aguarde...")
-            time.sleep(1)
-            print("")
-            print("Digite o texto / atalho:")
-            text = input()
-            cadastrar_tecla(key, text)
-
-        case 2:
-            print("Digite o atalho:")
-            key = input()
-            print("Digite o texto:")
-            text = input()
-            cadastrar_tecla(key, text)
-
-        case 3:
-            print("Digite a tecla 1:")
-            event = keyboard.read_event()
-            key_1 = event.name
-            time.sleep(0.5)
-            print(f"Tecla identificada: {key_1}!")
-            print("Aguarde...")
-            time.sleep(1)
-            print("")
-            print("Digite a tecla 2:")
-            event = keyboard.read_event()
-            key_2 = event.name
-            time.sleep(0.5)
-            print(f"Tecla identificada: {key_2}!")
-            print("Aguarde...")
-            time.sleep(1)
-            print("")
-            cadastrar_tecla(key_1, key_2)
-
-        case 0:
-            print("Cadastro cancelado...")
-            time.sleep(1)
-            print("Até mais!")
-
-    if escolha != 0:
-        print("Cadastro concluido com sucesso!")
-    print("---- Clique ESC para sair")
-    keyboard.wait("esc")
+    return chave
--- py/main.py
+++ py/main.py
@@ -1,14 +1,96 @@
 import keyboard
 import functions
+import time
 import json
 
-with open("py/config.json", "r", encoding="utf-8") as arquivo:
-    funcoes = json.load(arquivo)
+def carregar_atalhos():
 
-for i in funcoes:
-    tecla = i["key"]
-    text = i["post"]
-    keyboard.add_hotkey(tecla, lambda t=text: functions.func(t), suppress=True)
-keyboard.add_hotkey("f1", lambda: functions.rodar_terminal(), suppress=True)
+    try:
+        with open("py/config.json", "r", encoding="utf-8") as arquivo:
+            funcoes = json.load(arquivo)
 
+        for i in funcoes:
+            tecla = i["key"]
+            text = i["post"]
+            keyboard.add_hotkey(tecla, lambda t=text: functions.func(t), suppress=True)
+
+    except (FileNotFoundError, json.JSONDecodeError):
+        print("Arquivo config.json não encontrado.")
+
+def rodar_terminal():
+    print("+===============================+")
+    print("|      *    CADASTRAR    *      |")
+    print("|===============================|")
+    print("| Como deseja cadastrar?        |")
+    print("|-------------------------------|")
+    print("|  1- Tecla --> Texto / Atalho  |")
+    print("|  2- Atalho --> Texto          |")
+    print("|  3- REMAP                     |")
+    print("|  0- Cancelar                  |")
+    print("|-------------------------------|")
+    print("+===============================+")
+    print('  ------#OBS: O atalho deve seguir o seguinte formato: "key1+key2"')
+    print("")
+
+    while True:
+        print("Escolha uma opção:")
+        try:
+            escolha = int(input())
+            if 0 <= escolha <= 3:
+                break
+            print("Opção inexistente!")
+            print("digite novamente.....")
+            print("")
+        except ValueError:
+            print("Digite apenas um número entre 0 e 3!\n")
+
+    match escolha:
+        case 1:
+            print("Aguarde...")
+            key = functions.leitura_tecla()
+            print("Aguarde...")
+            time.sleep(1)
+            print("")
+            print("Digite o texto / atalho:")
+            text = input()
+            functions.cadastrar_tecla(key, text)
+            keyboard.add_hotkey(key, lambda t=text: functions.func(t), suppress=True)
+
+        case 2:
+            print("Digite o atalho:")
+            key = input()
+            print("Digite o texto:")
+            text = input()
+            functions.cadastrar_tecla(key, text)
+            keyboard.add_hotkey(key, lambda t=text: functions.func(t), suppress=True)
+
+        case 3:
+            print("Aguarde...")
+            key_1 = functions.leitura_tecla()
+            print("")
+            print("Aguarde...")
+            key_2 = functions.leitura_tecla()
+            print("Quase lá...")
+            time.sleep(1)
+            print("")
+            functions.cadastrar_tecla(key_1, key_2)
+            keyboard.add_hotkey(key_1, lambda t=key_2: functions.func(t), suppress=True)
+
+        case 0:
+            print("Cadastro cancelado...")
+            time.sleep(1)
+            print("Até mais!")
+
+    if escolha != 0:
+        print("Cadastro concluido com sucesso!")
+
+carregar_atalhos()
+
+keyboard.add_hotkey("ctrl+shift+k", rodar_terminal, suppress=True)
+
+print("================================================================")
+print("===  SISTEMA INICIADO  =========================================")
+print("=== Pressione [Ctrl + Shift + K] a qualquer momento para abrir o menu ")
+print("=== Pressione [ESC] para encerrar o programa.")
+print("================================================================")
 keyboard.wait("esc")
